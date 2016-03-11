16820e036a9ad992b0ef989f56e1b434577f1895
Lucas Bonomi <lucas.bonomi@gmail.com>
2016-03-11 14:34:12 +0100

    New template

--- src/lib/templates.js
+++ src/lib/templates.js
@@ -1,6 +1,32 @@
 let prefix = 'algolia-docsearch';
 let suggestionPrefix = `${prefix}-suggestion`;
-let footerPrefix = `${prefix}-footer`;
+let headerPrefix = `${prefix}-header`;
+
+// let templates = {
+//   suggestion: `
+//   <div class="${suggestionPrefix}
+//     {{#isCategoryHeader}}${suggestionPrefix}__main{{/isCategoryHeader}}
+//     {{#isSubCategoryHeader}}${suggestionPrefix}__secondary{{/isSubCategoryHeader}}
+//   ">
+//     <div class="${suggestionPrefix}--category-header">{{{category}}}</div>
+//     <div class="${suggestionPrefix}--wrapper">
+//       <div class="${suggestionPrefix}--subcategory-column">
+//         <span class="${suggestionPrefix}--subcategory-column-text">{{{subcategory}}}</span>
+//       </div>
+//       <div class="${suggestionPrefix}--content">
+//         <div class="${suggestionPrefix}--subcategory-inline">{{{subcategory}}}</div>
+//         <div class="${suggestionPrefix}--title">{{{title}}}</div>
+//         <div class="${suggestionPrefix}--text">{{{text}}}</div>
+//       </div>
+//     </div>
+//   </div>
+//   `,
+//   footer: `
+//     <div class="${footerPrefix}">
+//       Search by <a class="${footerPrefix}--logo" href="https://www.algolia.com/docsearch">Algolia</a>
+//     </div>
+//   `
+// };
 
 let templates = {
   suggestion: `
@@ -8,22 +34,33 @@ let templates = {
     {{#isCategoryHeader}}${suggestionPrefix}__main{{/isCategoryHeader}}
     {{#isSubCategoryHeader}}${suggestionPrefix}__secondary{{/isSubCategoryHeader}}
   ">
-    <div class="${suggestionPrefix}--category-header">{{{category}}}</div>
-    <div class="${suggestionPrefix}--wrapper">
-      <div class="${suggestionPrefix}--subcategory-column">
-        <span class="${suggestionPrefix}--subcategory-column-text">{{{subcategory}}}</span>
+    <div class="${suggestionPrefix}--item">
+      <div class="${suggestionPrefix}--item-header">
+        <h3>{{{category}}}</h3>
       </div>
       <div class="${suggestionPrefix}--content">
-        <div class="${suggestionPrefix}--subcategory-inline">{{{subcategory}}}</div>
-        <div class="${suggestionPrefix}--title">{{{title}}}</div>
-        <div class="${suggestionPrefix}--text">{{{text}}}</div>
+        <div class="${suggestionPrefix}--item-content--row">  
+          <div class="${suggestionPrefix}--item-content root-item col-3 no-padding-left">
+            <div class="item-content--subtitle">
+              <h3>{{{subcategory}}}</h3>
+            </div>
+          </div>
+          <div class="${suggestionPrefix}--item-content col-7 divider-left">
+            <div class="item-content--title">
+              <h3>{{{title}}}</h3>
+            </div>
+            <div class="item-content--description">
+              <p>{{{text}}}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
   `,
-  footer: `
-    <div class="${footerPrefix}">
-      Search by <a class="${footerPrefix}--logo" href="https://www.algolia.com/docsearch">Algolia</a>
+  header: `
+    <div class="${headerPrefix}">
+      Search by <a class="${headerPrefix}--logo" href="https://www.algolia.com/docsearch">Algolia</a>
     </div>
   `
 };
